c456be89be693efc04bfa3b59f1080a4881ac543
Nathaniel Richand <nrichand@gmail.com>
2020-12-11 10:25:22 +0100

    fix contrib

--- js/tribu.js
+++ js/tribu.js
@@ -251,6 +251,36 @@ function extractAllDatesAsHTML(doc){
 }
 
 //Pour afficher une page de contenu venant de prismic
+function displayContributionDetails(){
+    Helpers.withPrismic(function(ctx) {
+
+        // Retrieve the document
+        var uid = Helpers.queryString['uid'];
+        var id = Helpers.queryString['id'];
+
+        ctx.api.form("everything").ref(ctx.ref).query('[[:d = at(my.contribution.uid, "' + uid + '")]]').submit(function(err, docs) {
+
+            if (err) { Configuration.onPrismicError(err); return; }
+
+            var doc = docs.results[0];
+
+            // If there is no documents for this id
+            if(!doc) {
+                document.location = '404.html';
+            }
+
+            var titre = doc.getStructuredText('contribution.titre').asText();
+
+            document.title = "AgileTribu - " + titre;
+            $("#contribution_title").text(titre);
+            $("#contribution-detail").html(doc.getStructuredText('contribution.contenu').asHtml());
+
+        });
+    });
+
+}
+
+//Pour afficher une page de contenu venant de prismic
 function displayFormationDetails(){
     Helpers.withPrismic(function(ctx) {
 
